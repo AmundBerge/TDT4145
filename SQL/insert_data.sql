5084e09630523ce75d988db371964664bf5604cd
Aurora Meilian Karlsen <auroramk@stud.ntnu.no>
2025-03-15 04:48:03 +0100

    startet på brukstilfeller

---
+++ SQL/insert_data.sql
@@ -0,0 +1,10 @@
+--Brukstilfelle 1
+INSERT INTO Flyplass (flyplasskode, navn) VALUES
+( 'BOO', 'Bodø Lufthavn'), 
+( 'BGO', 'Bergen Lufthavn Flesland'), 
+('OSL', 'Oslo Lufthavn Gardermoen'), 
+( 'SVG', 'Stavanger Lufthavn Sola'), 
+( 'TRD', 'Trondheim Lufthavn Værnes');
+
+
+--Brukstilfelle 2
