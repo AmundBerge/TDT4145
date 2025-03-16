0037769acc1e483727a07447e23fbced06e10f6f
Aurora Meilian Karlsen <auroramk@stud.ntnu.no>
2025-03-16 17:34:28 +0100

    brukstilfelle 5 og oppretttelse av flyDB

---
+++ SQL/brukstilfelle5.sql
@@ -0,0 +1,15 @@
+--Brukstilfelle 5 
+SELECT 
+    Flyselskap.navn, 
+    Fly.flytypeNavn,
+    COUNT(Fly.registreringsnummer) AS antall_fly
+    
+FROM 
+    Flyselskap INNER JOIN Fly ON Flyselskap.flyselskapskode = Fly.flyselskapskode
+
+GROUP BY 
+    Flyselskap.navn, fly.flytypeNavn
+;
+
+
+
