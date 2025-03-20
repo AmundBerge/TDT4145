a8977eb3f1a25c83f2a28612c6a89212e4bce61c
Markus Helgø Wathle <markuhw@stud.ntnu.no>
2025-03-20 21:45:25 +0100

    kdhhgdjq

---
+++ SQL/insert_seats.sql
@@ -0,0 +1,46 @@
+
+INSERT INTO Sete (radNummer, seteBokstav, erLedig) VALUES
+
+-- Boeing 737 800
+
+-- Airbus a320neo
+
+-- Dash-8 100
+(1, 'C', TRUE),
+(1, 'D', TRUE),
+(2, 'A', TRUE),
+(2, 'B', TRUE),
+(2, 'C', TRUE),
+(2, 'D', TRUE),
+(3, 'A', TRUE),
+(3, 'B', TRUE),
+(3, 'C', TRUE),
+(3, 'D', TRUE),
+(4, 'A', TRUE),
+(4, 'B', TRUE),
+(4, 'C', TRUE),
+(4, 'D', TRUE),
+(5, 'A', TRUE),
+(5, 'B', TRUE),
+(5, 'C', TRUE),
+(5, 'D', TRUE),
+(6, 'A', TRUE),
+(6, 'B', TRUE),
+(6, 'C', TRUE),
+(6, 'D', TRUE),
+(7, 'A', TRUE),
+(7, 'B', TRUE),
+(7, 'C', TRUE),
+(7, 'D', TRUE),
+(8, 'A', TRUE),
+(8, 'B', TRUE),
+(8, 'C', TRUE),
+(8, 'D', TRUE),
+(9, 'A', TRUE),
+(9, 'B', TRUE),
+(9, 'C', TRUE),
+(9, 'D', TRUE),
+(10, 'A', TRUE),
+(10, 'B', TRUE),
+(10, 'C', TRUE),
+(10, 'D', TRUE);
